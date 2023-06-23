e39c16a559f5046b4dbb53ff8a98a8704d6febb4
Philip Laskowicz <philip.laskowicz@web.de>
2023-06-23 23:54:22 +0200

    add files for ex 10 nb 9

--- e2ml/evaluation/__init__.py
+++ e2ml/evaluation/__init__.py
@@ -1,10 +1,12 @@
 from ._loss_functions import zero_one_loss, binary_cross_entropy_loss
 from ._visualization import plot_decision_boundary
 from ._performance_measures import *
+from ._performance_measures_II import *
 
 __all__ = [
     "zero_one_loss",
     "binary_cross_entropy_loss",
     "plot_decision_boundary",
-    "_performance_measures"
+    "_performance_measures",
+    "_performance_measures_II",
 ]
